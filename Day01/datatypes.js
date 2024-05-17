a3ebd973fe838c41edde5b6fd87c861335dc1058
Ayodeji <amosayo16@gmail.com>
2024-05-17 16:09:00 +0100

    Answer

---
+++ Day01/datatypes.js
@@ -0,0 +1,12 @@
+
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let name = "John";
+let isClosed = false;
+let score ;
+let mark = null;
+
+console.log (typeof name);
+console.log (typeof isClosed);
+console.log (typeof score);
+console.log (typeof mark);
---
+++ Day01/variable.js
@@ -0,0 +1,42 @@
+
+//     4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types.
+
+let discipline = "computer Science";
+let isRaining = false;
+let address ;
+let memberId = null;
+
+//      6. Declare four variables without assigning values
+
+let LastName ;
+let occupation ;
+let gender ;
+let state ;
+
+//      7. Declare four variables with assigned values
+
+let MiddleName = "Grace";
+let id = 345;
+let yourgender  = "Female";
+let City = "Lekki" ;
+
+//      8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let FirstName = "Grace";
+let lastName = 25;
+let maritalStatus  = "Female";
+let country = "Lekki" ;
+let age = 23
+
+//      9. Declare variables to store your first name, last name, marital status, country and age in a single line
+
+
+let FName = "Grace", lName = 25, mStatus  = "Female", Country = "Lekki", Age = 23
+
+//      10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 24;
+let yourAge =26;
+
+console.log (myAge);
+console.log (yourAge);
